b505ee9ff33920e16e30e083e5e579ea27613b90
Donghyeon Lee <zuzudnf@gmail.com>
2021-06-30 20:46:37 +0900

    net ninja #24 done

--- netNinja/dojo-blog/src/Home.js
+++ netNinja/dojo-blog/src/Home.js
@@ -1,41 +1,15 @@
-import { useState, useEffect } from "react";
+import useFetch from "./useFetch";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    {
-      title: "my new website",
-      body: "lorem ipsum ...",
-      author: "donglee",
-      id: 1
-    },
-    {
-      title: "welcome party!",
-      body: "lorem ipsum ...",
-      author: "morello",
-      id: 2
-    },
-    {
-      title: "Web dev top tips",
-      body: "lorem ipsum ...",
-      author: "morello",
-      id: 3
-    }
-  ]);
-
-  const handleDelete = id => {
-    const newBlogs = blogs.filter(blog => blog.id !== id);
-    setBlogs(newBlogs);
-  };
-
-  useEffect(() => {
-    console.log("useeffect run!");
-    console.log(blogs);
-  });
-
+  const { data: blogs, isPending, error } = useFetch(
+    "http://localhost:8000/blogs"
+  );
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading...</div>}
+      {blogs && <BlogList blogs={blogs} title="All Blogs" />}
     </div>
   );
 };
